c17430c032f3eaf877d4845b496c34d0c61cf786
Carlos Pons <karlospn@gmail.com>
2017-06-21 23:57:18 +0200

    Testing how to respond to basic user events

--- Growler/growler-vue.js
+++ Growler/growler-vue.js
@@ -7,6 +7,35 @@
                     changeQuery: '',
                     searchIndexes: [],
                     searchIndex: 'ValueA',
-                    selectedSearchIndex: 'ValueA'
+                    selectedSearchIndex: 'ValueA',
+                    keypressModel: ''
+                },
+                methods: {
+                    executeSearch: function(t, event) {
+                        alert("Event: " + event + " Event target:" + event.target);
+                        alert("Parameter: "+ t);
+                    },
+                    grandparentClick: function(){
+                        alert("Grandparent");
+                    },
+                    parentClick: function(){
+                        alert("Parent");
+                    },
+                    childClick: function(){
+                        alert("Child");
+                    },
+                    evaluateKey: function(){
+                        alert("Key press!");
+                    },
+                   evaluateCustomKeyEvent: function(){
+                        alert("F1 press!");
+                    },
+                    MouseLeftClick: function(){
+                        alert("Left Click!!");
+                    }
                 }
             });
+
+Vue.config.keyCodes = {
+    f1: 112   
+};
